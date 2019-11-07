f78c8ccf3846820c69df2468b191818b23a63da1
Christopher Calvin <42115505+chriscalvinn@users.noreply.github.com>
2019-11-08 00:06:50 +0700

    second commit
    add random package, random-name package, insert to mahasiswa table

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+//framework
 const express = require('express')
 const app = express()
+
+//random
+const random = require('random')
+var randomname = require('random-name')
 
 //database
 var mysql = require('mysql');
@@ -9,7 +14,8 @@ var connection = mysql.createConnection({
   password : '',
   database : 'perpus_online'
 });
- 
+
+//Database connection
 connection.connect(function(err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
@@ -18,9 +24,33 @@ connection.connect(function(err) {
  
   console.log('connected as id ' + connection.threadId);
 });
+
+//Insert mahasiswa
+function insertMahasiswa(){
+	//variable
+	var nim = random.int(min = 18216001, max = 18218045)
+	var name = randomname.first().concat(randomname.middle())
+	var email = name.concat('@gmail.com')
+	
+	//looping
+	var i;
+	for (i = 0; i < 10; i++) {
+		connection.query("INSERT INTO mahasiswa (NIM,Nama_Mahasiswa,Email) Values (?,?,?)",[nim,name,email], function (error, results, fields) {
+			if (error) throw error;
+			console.log(results);
+			// error will be an Error if one occurred during the query
+			// results will contain the results of the query
+			// fields will contain information about the returned results fields (if any)
+		});
+		nim = random.int(min = 18216001, max = 18218045)
+		name = randomname.first().concat(randomname.middle())
+		email = name.concat('@gmail.com')
+	}
+} 
  
 app.get('/', function (req, res) {
   res.send('Hello World')
+  insertMahasiswa()
   connection.query('SELECT * From buku', function (error, results, fields) {
 	  if (error) throw error;
 	  console.log(results);
