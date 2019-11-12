23e2c48de26397e701cd92e2a4d914929d2954e6
Christopher Calvin <42115505+chriscalvinn@users.noreply.github.com>
2019-11-12 21:54:57 +0700

    second commit
    getter all

--- index.js
+++ index.js
@@ -1,63 +1,158 @@
-//framework
-const express = require('express')
-const app = express()
-
-//random
-const random = require('random')
-var randomname = require('random-name')
-
-//database
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'perpus_online'
-});
-
-//Database connection
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
- 
-  console.log('connected as id ' + connection.threadId);
-});
-
-//Insert mahasiswa
-function insertMahasiswa(){
-	//variable
-	var nim = random.int(min = 18216001, max = 18218045)
-	var name = randomname.first().concat(randomname.middle())
-	var email = name.concat('@gmail.com')
+//framework	
+const express = require('express')	
+const bodyparser = require('body-parser')	
+const app = express()	
 	
-	//looping
-	var i;
-	for (i = 0; i < 10; i++) {
-		connection.query("INSERT INTO mahasiswa (NIM,Nama_Mahasiswa,Email) Values (?,?,?)",[nim,name,email], function (error, results, fields) {
+app.use(bodyparser.json());	
+	
+//database	
+var mysql = require('mysql');	
+var connection = mysql.createConnection({	
+  host     : 'localhost',	
+  user     : 'root',	
+  password : '',	
+  database : 'perpus_online'	
+});	
+	
+//Database connection	
+connection.connect(function(err) {	
+  if (err) {	
+    console.error('error connecting: ' + err.stack);	
+    return;	
+  }	
+	
+  console.log('connected as id ' + connection.threadId)	
+});	
+	
+	
+	
+app.get('/book', function (req, res) {	
+	try{
+		connection.query('SELECT * From buku', function (error, results, fields) {
 			if (error) throw error;
-			console.log(results);
+			//console.log(results);
+			res.json(results);
 			// error will be an Error if one occurred during the query
 			// results will contain the results of the query
 			// fields will contain information about the returned results fields (if any)
-		});
-		nim = random.int(min = 18216001, max = 18218045)
-		name = randomname.first().concat(randomname.middle())
-		email = name.concat('@gmail.com')
+		})
+	} catch(err) {
+		console.log(err)
+		res.json({"response-code":500,"message":"Internal server error"})
 	}
-} 
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-  insertMahasiswa()
-  connection.query('SELECT * From buku', function (error, results, fields) {
-	  if (error) throw error;
-	  console.log(results);
-	// error will be an Error if one occurred during the query
-	// results will contain the results of the query
-	// fields will contain information about the returned results fields (if any)
-  });
-})
+});
+
+app.get('/book/:Judul', function (req, res) {
+	try{
+		var query = 'SELECT * FROM buku WHERE Judul_Buku = ?'
+		connection.query(query, (req.params.Judul), function (error, results, fields) {
+			if (error) throw error;
+			//console.log(results);
+			res.json(results);
+		})
+	} catch(err) {
+		console.log(err)
+		res.json({"response-code":500,"message":"Internal server error"})
+	}
+});
+
+app.get('/jurnal', function (req, res) {	
+	try{
+		connection.query('SELECT * From jurnal', function (error, results, fields) {
+			if (error) throw error;
+			//console.log(results);
+			res.json(results);
+			// error will be an Error if one occurred during the query
+			// results will contain the results of the query
+			// fields will contain information about the returned results fields (if any)
+		})
+	} catch(err) {
+		console.log(err)
+		res.json({"response-code":500,"message":"Internal server error"})
+	}
+});
+
+app.get('/jurnal/:id', function (req, res) {	
+	try{
+		var query = 'SELECT * From jurnal WHERE IDJurnal = ?'
+		connection.query(query, (req.params.id), function (error, results, fields) {
+			if (error) throw error;
+			//console.log(results);
+			res.json(results);
+			// error will be an Error if one occurred during the query
+			// results will contain the results of the query
+			// fields will contain information about the returned results fields (if any)
+		})
+	} catch(err) {
+		console.log(err)
+		res.json({"response-code":500,"message":"Internal server error"})
+	}
+});
+
+app.get('/mahasiswa', function (req, res) {	
+	try{
+		connection.query('SELECT * From mahasiswa', function (error, results, fields) {
+			if (error) throw error;
+			//console.log(results);
+			res.json(results);
+			// error will be an Error if one occurred during the query
+			// results will contain the results of the query
+			// fields will contain information about the returned results fields (if any)
+		})
+	} catch(err) {
+		console.log(err)
+		res.json({"response-code":500,"message":"Internal server error"})
+	}
+});
+
+app.get('/mahasiswa/:nim', function (req, res) {	
+	try{
+		var query = 'SELECT * From mahasiswa WHERE NIM = ?'
+		connection.query(query, (req.params.nim), function (error, results, fields) {
+			if (error) throw error;
+			//console.log(results);
+			res.json(results);
+			// error will be an Error if one occurred during the query
+			// results will contain the results of the query
+			// fields will contain information about the returned results fields (if any)
+		})
+	} catch(err) {
+		console.log(err)
+		res.json({"response-code":500,"message":"Internal server error"})
+	}
+});
+
+app.get('/peminjaman', function (req, res) {	
+	try{
+		connection.query('SELECT * From peminjaman', function (error, results, fields) {
+			if (error) throw error;
+			//console.log(results);
+			res.json(results);
+			// error will be an Error if one occurred during the query
+			// results will contain the results of the query
+			// fields will contain information about the returned results fields (if any)
+		})
+	} catch(err) {
+		console.log(err)
+		res.json({"response-code":500,"message":"Internal server error"})
+	}
+});
+
+app.get('/peminjaman/:id', function (req, res) {	
+	try{
+		var query = 'SELECT * From peminjaman WHERE IDPeminjaman = ?'
+		connection.query(query, (req.params.id), function (error, results, fields) {
+			if (error) throw error;
+			//console.log(results);
+			res.json(results);
+			// error will be an Error if one occurred during the query
+			// results will contain the results of the query
+			// fields will contain information about the returned results fields (if any)
+		})
+	} catch(err) {
+		console.log(err)
+		res.json({"response-code":500,"message":"Internal server error"})
+	}
+});
  
 app.listen(3000)
