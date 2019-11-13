49fc5279b6b4747d54be8b6132aa1a101943ee45
Christopher Calvin <42115505+chriscalvinn@users.noreply.github.com>
2019-11-14 00:55:36 +0700

    revisi users
    penambahan method post dan put

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
+
 //framework
 const express = require('express')
 const app = express()
-
+const bodyparser = require('body-parser')
 const http = require('http')
+
+app.use(bodyparser.json());	
+app.use(bodyparser.urlencoded({ extended: false }));
 
 //database	
 var mysql = require('mysql');	
@@ -22,25 +26,8 @@ connection.connect(function(err) {
 	
   console.log('connected as id ' + connection.threadId)	
 });	
-	
-	
-	
-// app.get('/book', function (req, res) {	
-// 	try{
-// 		connection.query('SELECT * From buku', function (error, results, fields) {
-// 			if (error) throw error;
-// 			//console.log(results);
-// 			res.json(results);
-// 			// error will be an Error if one occurred during the query
-// 			// results will contain the results of the query
-// 			// fields will contain information about the returned results fields (if any)
-// 		})
-// 	} catch(err) {
-// 		console.log(err)
-// 		res.json({"response-code":500,"message":"Internal server error"})
-// 	}
-// });
 
+//get buku
 app.get('/book', function (req, res) {
 	try{
         var judul = req.query.judul
@@ -69,7 +56,9 @@ app.get('/book', function (req, res) {
             })
         }
         else {
-            console.log('wrong')
+            let ret = {
+                
+            }
         }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 	} catch(err) {
 		console.log(err)
@@ -77,48 +66,52 @@ app.get('/book', function (req, res) {
 	}
 });
 
+//get jurnal
 app.get('/jurnal', function (req, res) {	
 	try{
-		connection.query('SELECT * From jurnal', function (error, results, fields) {
-			if (error) throw error;
-			//console.log(results);
-			res.json(results);
-			// error will be an Error if one occurred during the query
-			// results will contain the results of the query
-			// fields will contain information about the returned results fields (if any)
-		})
+		var judul = req.query.judul
+		var author = req.query.pengarang
+		if ((judul==null)&&(author==null)){
+            connection.query('SELECT * From jurnal', function (error, results, fields) {
+                if (error) throw error;
+                //console.log(results);
+                res.json(results);
+            })
+        }
+        else if ((judul!=null)&&(author==null)) {
+            var query = 'SELECT * FROM jurnal WHERE Judul_Jurnal = ?'
+		    connection.query(query, (judul), function (error, results, fields) {
+			    if (error) throw error;
+			    //console.log(results);
+			    res.json(results);
+            })
+        }
+        else if ((judul==null)&&(author!=null)){
+            var query = 'SELECT * FROM jurnal WHERE Pengarang_Jurnal = ?'
+		    connection.query(query, (author), function (error, results, fields) {
+			    if (error) throw error;
+			    //console.log(results);
+                res.json(results);
+            })
+        }
+        else {
+            let ret = {
+                
+            }
+        }   
 	} catch(err) {
 		console.log(err)
 		res.json({"response-code":500,"message":"Internal server error"})
 	}
 });
 
-app.get('/jurnal/:id', function (req, res) {	
-	try{
-		var query = 'SELECT * From jurnal WHERE IDJurnal = ?'
-		connection.query(query, (req.params.id), function (error, results, fields) {
-			if (error) throw error;
-			//console.log(results);
-			res.json(results);
-			// error will be an Error if one occurred during the query
-			// results will contain the results of the query
-			// fields will contain information about the returned results fields (if any)
-		})
-	} catch(err) {
-		console.log(err)
-		res.json({"response-code":500,"message":"Internal server error"})
-	}
-});
-
+//get mahasiswa
 app.get('/mahasiswa', function (req, res) {	
 	try{
 		connection.query('SELECT * From mahasiswa', function (error, results, fields) {
 			if (error) throw error;
 			//console.log(results);
 			res.json(results);
-			// error will be an Error if one occurred during the query
-			// results will contain the results of the query
-			// fields will contain information about the returned results fields (if any)
 		})
 	} catch(err) {
 		console.log(err)
@@ -126,6 +119,7 @@ app.get('/mahasiswa', function (req, res) {
 	}
 });
 
+//get mahasiswa by nim
 app.get('/mahasiswa/:nim', function (req, res) {	
 	try{
 		var query = 'SELECT * From mahasiswa WHERE NIM = ?'
@@ -133,9 +127,6 @@ app.get('/mahasiswa/:nim', function (req, res) {
 			if (error) throw error;
 			//console.log(results);
 			res.json(results);
-			// error will be an Error if one occurred during the query
-			// results will contain the results of the query
-			// fields will contain information about the returned results fields (if any)
 		})
 	} catch(err) {
 		console.log(err)
@@ -143,15 +134,13 @@ app.get('/mahasiswa/:nim', function (req, res) {
 	}
 });
 
+//get peminjaman
 app.get('/peminjaman', function (req, res) {	
 	try{
 		connection.query('SELECT * From peminjaman', function (error, results, fields) {
 			if (error) throw error;
 			//console.log(results);
 			res.json(results);
-			// error will be an Error if one occurred during the query
-			// results will contain the results of the query
-			// fields will contain information about the returned results fields (if any)
 		})
 	} catch(err) {
 		console.log(err)
@@ -159,6 +148,7 @@ app.get('/peminjaman', function (req, res) {
 	}
 });
 
+//Get Peminjaman by id
 app.get('/peminjaman/:id', function (req, res) {	
 	try{
 		var query = 'SELECT * From peminjaman WHERE IDPeminjaman = ?'
@@ -166,11 +156,87 @@ app.get('/peminjaman/:id', function (req, res) {
 			if (error) throw error;
 			//console.log(results);
 			res.json(results);
-			// error will be an Error if one occurred during the query
-			// results will contain the results of the query
-			// fields will contain information about the returned results fields (if any)
 		})
 	} catch(err) {
+		console.log(err)
+		res.json({"response-code":500,"message":"Internal server error"})
+	}
+});
+
+//POST Data Buku Baru
+app.post('/book',function(req,res){
+	try{
+		var query = 'INSERT INTO buku (ISBN, Judul_Buku, Pengarang, Sinopsis, Penerbit, Tahun_Terbit, Kategoti, Stok) VALUES (?,?,?,?,?,?,?,?)'
+		var data = req.body
+		var instance = [data.ISBN,data.Judul_Buku,data.Pengarang,data.Sinopsis, data.Penerbit, data.Tahun_Terbit, data.Kategori, data.Stok]
+		connection.query(query, instance, function (error, results, fields) {
+			if (error) throw error;
+			//console.log(results);
+			res.json({"response-code":200,"message":"Record successfully added"})
+		})
+	} catch(err) {
+		console.log(err)
+		res.json({"response-code":500,"message":"Internal server error"})
+	}
+});
+
+//POST data Jurnal Baru
+app.post('/jurnal',function(req,res){
+	try{
+		var query = 'INSERT INTO jurnal (IDJurnal, Judul_Jurnal, Pengarang_Jurnal, Abstraksi, Status_e_jurnal, Tahun_Terbit_Jurnal, Kategori_Jurnal, Stok_Jurnal) VALUES (?,?,?,?,?,?,?,?)'
+		var data = req.body
+		var instance = [data.IDJurnal, data.Judul_Jurnal, data.Pengarang_Jurnal, data.Abstraksi, data.Status_e_jurnal, data.Tahun_Terbit_Jurnal, data.Kategori_Jurnal, data.Stok_Jurnal]
+		connection.query(query, instance, function (error, results, fields) {
+			if (error) throw error;
+			//console.log(results);
+			res.json({"response-code":200,"message":"Record successfully added"})
+		})
+	} catch(err) {
+		console.log(err)
+		res.json({"response-code":500,"message":"Internal server error"})
+	}
+});
+
+//PUT data stok buku
+app.put('/book',function(req,res){
+	try{
+		var judul = req.query.judul
+        var ISBN = req.query.ISBN
+		if ((judul!=null)&&(ISBN==null)) {
+            var query = 'UPDATE buku SET Stok_Buku=? WHERE Judul_Buku=?'
+            var data = [req.body.Stok_Buku, judul]
+		    connection.query(query, data, function (error, results, fields) {
+			    if (error) throw error;
+			    //console.log(results);
+			    res.json({"response-code":200,"message":"Record successfully updated"});
+            })
+        }
+        else if ((judul==null)&&(ISBN!=null)){
+            var query = 'UPDATE buku SET Stok_Buku=? WHERE ISBN=?'
+            var data = [req.body.Stok_Buku, ISBN]
+		    connection.query(query, data, function (error, results, fields) {
+			    if (error) throw error;
+			    //console.log(results);
+			    res.json({"response-code":200,"message":"Record successfully updated"});
+            })
+		}
+	} catch(err){
+		console.log(err)
+		res.json({"response-code":500,"message":"Internal server error"})
+	}
+});
+
+app.put('/peminjaman',function(req,res){
+	try{
+		var id = req.query.id
+		var query = 'UPDATE peminjaman SET Status_Pengembalian=? WHERE IDPeminjaman=?'
+		var data = [req.body.Status_Pengembalian, id]
+		connection.query(query, data, function (error, results, fields){
+			if (error) throw error;
+			//console.log(results);
+			res.json({"response-code":200,"message":"Record successfully updated"});
+		})
+	} catch(err){
 		console.log(err)
 		res.json({"response-code":500,"message":"Internal server error"})
 	}
@@ -188,9 +254,6 @@ function checkactive(nim){
 			//console.log(results);
             var res = results;
             return (res!=null)
-			// error will be an Error if one occurred during the query
-			// results will contain the results of the query
-			// fields will contain information about the returned results fields (if any)
 		})
 	} catch(err) {
 		console.log(err)
@@ -215,15 +278,10 @@ function pinjam (tipe,nim,id) {
             }
         }
     }
-    
 }
 
 //function check tanggal
-//app.put for changing active status
 //app.post for adding new peminjaman
 //app.get for get peminjaman by nim
-//app.get for get books by author
-//app.get for get jurnal by judul
-//app.get for get jurnal by author
 
 module.exports = app;
