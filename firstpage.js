37db0fd470baf9c15720c9cd79c3ea6861bd5c6b
sashagambetta <sashagambettaa@gmail.com>
2019-11-14 13:14:43 +0700

    updated first page & login

--- firstpage.js
+++ firstpage.js
@@ -59,12 +59,3 @@ function showborrow() {
         }
     }
 }
-
-//LOGIN 
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
-    
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
