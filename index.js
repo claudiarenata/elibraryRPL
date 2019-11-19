b89ef6e5a3ed6fede723ab40435e17f1a825db2a
sashagambetta <sashagambettaa@gmail.com>
2019-11-19 15:12:48 +0700

    updated index.js dan cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // framework	
 const express = require('express')	
 const bodyparser = require('body-parser')	
+const cors = require('cors')
 const app = express()	
 
 let userRoute = require('./routes/users')
@@ -8,9 +9,11 @@ let adminRoute = require('./routes/admin')
 let func = require('./routes/function')
 
 
+
 app.use(bodyparser.json());	
 app.use(userRoute)
 app.use(adminRoute)
 app.use(func)
+app.use(cors())
 
 app.listen(3000)
