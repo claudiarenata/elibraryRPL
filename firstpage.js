a4e840cc9eac6155940e865be125a075b67a3f9b
sashagambetta <sashagambettaa@gmail.com>
2019-11-21 20:16:17 +0700

    yawlo

--- firstpage.js
+++ firstpage.js
@@ -86,6 +86,6 @@ function openTable(evt, tabName) {
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById('tabName').style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
