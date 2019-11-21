895298b7f9efb99747c88b7d0759a7c46fb6dc91
sashagambetta <sashagambettaa@gmail.com>
2019-11-21 22:51:52 +0700

    UYE

--- routes/users.js
+++ routes/users.js
@@ -326,7 +326,7 @@ app.post('/login/admin',function(req,res){
 			    //console.log(results);
 			    let data = results
 			    if (func.isEmpty(data)){
-					res.code(401);
+					res.status(401);
 					res.json({"response-code":401,"message":"Unauthorized"});
 				} else {
 					res.json({"response-code":200,"message":"Authorized"});
@@ -356,7 +356,7 @@ app.post('/login/user',function(req,res){
 			    //console.log(results);
 				let data = results;
 			    if (func.isEmpty(data)){
-					res.code(401);
+					res.status(401);
 					res.json({"response-code":401,"message":"Unauthorized"});
 				} else {
 					res.json({"response-code":200,"message":"Authorized"});
